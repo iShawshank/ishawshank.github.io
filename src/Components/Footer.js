5ae77f2e5816d1d4ac17ce2cecde1b29e5077664
Kevin Shaw <ishawshank39@gmail.com>
2020-05-22 19:11:31 -0600

    remove contact section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,17 +7,30 @@ class Footer extends Component {
       var networks = this.props.data.social.map(function (network) {
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
+
+      var name = this.props.data.name;
+      var phone = this.props.data.phone;
+      var email = this.props.data.email;
     }
 
     return (
       <footer>
 
         <div className="row">
-          <div className="twelve columns">
+          <div className="four columns">
             <ul className="social-links">
               {networks}
             </ul>
+          </div>
+          <div className="four columns">
 
+          </div>
+          <div className="four columns">
+            <p className="address">
+              {name}<br />
+              {email} <br />
+              <span>{phone}</span>
+            </p>
           </div>
           <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
         </div>

5f65fc83450d5ba32024e60b3cf0823047d0b594
Kevin Shaw <ishawshank@ishawshanks-MacBook-Pro.local>
2023-10-25 15:13:26 -0600

    update site header

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,74 +1,131 @@
-import React, { useState } from 'react'
-import './NavBar.css'
+import React, { useState } from 'react';
+import './NavBar.css';
 import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
   const [showMenu, setShowMenu] = useState(false);
-  
+
   return (
-    <nav className='navContainer'>
-      <div className='navbar'>
-        <h1 className='headerName'>Kevin Shaw</h1>
-        <div className='desktopMenu hidden lg:flex'>
-          <Link activeClass='active' to='intro' spy={true} smooth={true} offset={0} duration={500} className='desktopMenuListItem'>Home</Link>
-          <Link activeClass='active' to='skills' spy={true} smooth={true} offset={0} duration={500} className='desktopMenuListItem '>Experience</Link>
-          <Link activeClass='active' to='projects' spy={true} smooth={true} offset={0} duration={500} className='desktopMenuListItem'>Projects</Link>
+    <nav className="navContainer">
+      <div className="navbar">
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+          id="headerName"
+        >
+          Kevin Shaw
+        </Link>
+        <div className="desktopMenu hidden lg:flex">
+          <Link
+            activeClass="active"
+            to="intro"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            className="desktopMenuListItem"
+          >
+            Home
+          </Link>
+          <Link
+            activeClass="active"
+            to="skills"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            className="desktopMenuListItem"
+          >
+            Experience
+          </Link>
+          <Link
+            activeClass="active"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            className="desktopMenuListItem"
+          >
+            Projects
+          </Link>
         </div>
 
         <div></div>
-        <button className='desktopMenuBtn hidden lg:flex' onClick={() => document.getElementById('contact').scrollIntoView({ behavior: 'smooth'})}>
-          <FontAwesomeIcon className='desktopMenuImg' icon='comment-dots' />
+        <button
+          className="desktopMenuBtn hidden lg:flex"
+          onClick={() =>
+            document
+              .getElementById('contact')
+              .scrollIntoView({ behavior: 'smooth' })
+          }
+        >
+          <FontAwesomeIcon
+            className="desktopMenuImg"
+            icon="comment-dots"
+          />
           Contact Me
         </button>
 
-        <FontAwesomeIcon className='mobMenuIcon lg:hidden pr-5' icon={faBars} onClick={() => setShowMenu(!showMenu)} />
-        <div className='mobileMenu bg-zinc-800 rounded' style={{ display: showMenu ? 'flex': 'none' }}>
-          <Link 
-            activeClass='active' 
-            to='intro' 
-            spy={true} 
-            smooth={true} 
-            offset={0} 
-            duration={500} 
-            className='desktopMenuListItem' 
+        <FontAwesomeIcon
+          className="mobMenuIcon lg:hidden pr-5"
+          icon={faBars}
+          onClick={() => setShowMenu(!showMenu)}
+        />
+        <div
+          className="mobileMenu bg-zinc-800 rounded"
+          style={{ display: showMenu ? 'flex' : 'none' }}
+        >
+          <Link
+            activeClass="active"
+            to="intro"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            className="desktopMenuListItem"
             onClick={() => setShowMenu(false)}
           >
             Home
           </Link>
-          <Link 
-            activeClass='active' 
-            to='skills' 
-            spy={true} 
-            smooth={true} 
-            offset={0} 
-            duration={500} 
-            className='desktopMenuListItem ' 
+          <Link
+            activeClass="active"
+            to="skills"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            className="desktopMenuListItem "
             onClick={() => setShowMenu(false)}
           >
             Experience
           </Link>
-          <Link 
-            activeClass='active' 
-            to='projects' 
-            spy={true} 
-            smooth={true} 
-            offset={0} 
-            duration={500} 
-            className='desktopMenuListItem' 
+          <Link
+            activeClass="active"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            className="desktopMenuListItem"
             onClick={() => setShowMenu(false)}
           >
             Projects
           </Link>
-          <Link 
-            activeClass='active' 
-            to='contact' 
-            spy={true} 
-            smooth={true} 
-            offset={0} 
-            duration={500}  
-            className='desktopMenuListItem' 
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            className="desktopMenuListItem"
             onClick={() => setShowMenu(false)}
           >
             Contact
@@ -76,7 +133,7 @@ const NavBar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;

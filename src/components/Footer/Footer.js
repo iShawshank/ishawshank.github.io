48c832d338c4d7fb2c45a795e2a442ea7f26600c
iShawshank <ishawshank39@gmail.com>
2024-05-01 08:46:22 -0600

    Update copyright date and fix mobile image issue

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
+  const currentDate = new Date();
   return (
-    <footer className='footer flex items-center justify-center text-sm font-thin w-full h-16 bg-zinc-800'>
-      Copyright &#169; 2023 Kevin Shaw
+    <footer className="footer flex items-center justify-center text-sm font-thin w-full h-16 bg-zinc-800">
+      Copyright &#169; {currentDate.getFullYear()} Kevin Shaw
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
